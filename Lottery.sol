e8373a816c3921ebc755e4058760f0f778559e1e
Raj Soni <115941128+rajsoni77@users.noreply.github.com>
2024-04-02 20:28:34 +0530

    Update Bank.sol

--- Lottery.sol
+++ Lottery.sol
@@ -1,40 +1,47 @@
-//SPDX-License-Identifier: GPL-3.0
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-pragma solidity >=0.5.0 <0.9.0;
+contract Banking{
+    mapping(address => uint) public balances;
+    address payable owner;
 
-contract Lottery{
-     address public manager;
-     address payable[] public players;
-     address payable public winner;
+    constructor() {
+        owner = payable(msg.sender);
+    }
 
-   constructor(){
-    manager= msg.sender;
-   }
-  
- function participate() public payable {
-
-  require(msg.value==1 ether, " Pls pay 1 ether only");
-  players.push(payable (msg.sender));
- }
-
- function getBalance() public view returns(uint) {
-  require(manager==msg.sender, "Youn are not manager");
-  return address(this).balance;
- }
-
- function random() public view returns(uint) {
-  return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,players.length)));
- }
+    function deposit() public payable{
+  require(msg.value > 0 , " Deposit amount should be more than 0");
+ balances[msg.sender] += msg.value;
+    }
  
- function pickWinner() public {
-  require(manager== msg.sender,"U are not manager");
-  require(players.length>=3," Players are less than 3");
+    function withdraw(uint amount) public{
+   require(msg.sender == owner , " Only owner can withdraw funds");
+   require(amount <= balances[msg.sender], "Insufficient funds!");
+   require(amount > 0, "Withdrawal amount must be more than 0");
+   payable(msg.sender).transfer(amount);
+   balances[msg.sender] -= amount;
+    }
 
- uint r=random();
-  uint index = r%players.length;
-  winner= players[index];
-  winner.transfer(getBalance());
-  players= new address payable[](0);
- }
- 
-}
+    function transfer(address payable recipient, uint amount) public{
+    require(amount<= balances[msg.sender],"Insufficient funds");
+    require(amount > 0, "Transfer amount much be more than 0");
+    balances[msg.sender] -= amount;
+    balances[recipient] += amount;
+    }
+
+    function getBalance(address payable user) public view returns (uint){
+    return balances[user];
+    }
+
+    function grantAccess(address payable user) public  {
+        require(msg.sender == owner, "Only owner can revoke access");
+        require(user != owner, "Cannot revoke access for the current owner");
+        owner =payable(msg.sender);
+    }
+
+    function destroy() public {
+    require(msg.sender == owner, "Only owner can destroy the contract");
+    owner.transfer(address(this).balance);
+    }
+
+    }
